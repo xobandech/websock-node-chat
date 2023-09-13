429fe60ad0b20cdbc193c4a732e6e9a89b10fbc8
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-09-13 17:42:41 +0200

    Add separating users for easier communication

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ const socket = new WebSocket("ws://localhost:8080");
 const button = document.getElementById("send-button");
 const messagesDiv = document.getElementById("chat-messages");
 const messageInput = document.getElementById("message-input");
+const userUid = new Date().getTime()
 
 button.addEventListener("click", () => {
-    socket.send(messageInput.value);
+    const message = messageInput.value;
+    socket.send(JSON.stringify({ message, userUid }));
+    messageInput.value = "";
 });
 
 socket.addEventListener("message", (event) => {
-    const msg = event.data;
-    console.log(msg);
-    messagesDiv.innerHTML += `<div>${msg}</div>`;
+    const data = JSON.parse(event.data);
+    console.log(data);
+    displayMessage({message: data.message, userId: data.userUid});
 });
+
+function displayMessage({ message, userId }) {
+    const messageDiv = document.createElement("div");
+    messageDiv.innerHTML += `<strong>${userId === userUid ? "You" : "Other"}:</strong> ${message}`;
+    messagesDiv.appendChild(messageDiv);
+}
