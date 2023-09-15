d01fd646396ed1a4bfd59255d466de9311209955
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-09-15 17:03:10 +0200

    Add userNickname handling

--- script.js
+++ script.js
@@ -2,22 +2,28 @@ const socket = new WebSocket("ws://localhost:8080");
 const button = document.getElementById("send-button");
 const messagesDiv = document.getElementById("chat-messages");
 const messageInput = document.getElementById("message-input");
+const nicknameInput = document.getElementById("nickname-input")
 const userUid = new Date().getTime()
-
+let userNickname = ""
 button.addEventListener("click", () => {
     const message = messageInput.value;
-    socket.send(JSON.stringify({ message, userUid }));
+    socket.send(JSON.stringify({ message, userUid, userNickname }));
     messageInput.value = "";
 });
 
 socket.addEventListener("message", (event) => {
     const data = JSON.parse(event.data);
     console.log(data);
-    displayMessage({message: data.message, userId: data.userUid});
+    displayMessage({message: data.message, userId: data.userUid, userNickname: data.userNickname});
 });
 
-function displayMessage({ message, userId }) {
+function displayMessage({ message, userId, userNickname }) {
+    console.log(userNickname);
     const messageDiv = document.createElement("div");
-    messageDiv.innerHTML += `<strong>${userId === userUid ? "You" : "Other"}:</strong> ${message}`;
+    messageDiv.innerHTML += `<strong>${userId === userUid ? "You" : userNickname}:</strong> ${message}`;
     messagesDiv.appendChild(messageDiv);
+}
+
+nicknameInput.oninput =function(e) {
+    userNickname = e.target.value
 }
